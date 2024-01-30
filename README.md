7f0e731f9ba6ae6c13d2412f48656ebd3748faa2
Fideltodayy <fidelotieno11@gmail.com>
2024-01-30 10:55:58 +0300

    Updated readme and connected to database

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
+I began setting up my project and installed dependancies that I was to us in my project.
+I then set up my MongoDB database and built a model using my mongoose ORM. I was to install mongoDB but chose to install mongoose as my ORM, an easier way to create schemas.
+
 ## Dependencies
+
 1. Express
 2. Mongoose
 3. Mocha
--- app.js
+++ app.js
@@ -3,13 +3,16 @@
 // import the express module
 const express = require("express");
 
+// import the mongoose module
+const mongoose = require("mongoose");
+
 // create an object of express
 const app = express();
 
 // middleware
 app.use(express.json());
 
-app.listen(3000, () => console.log("Listening on port 3000..."));
+app.listen(3001, () => console.log("Listening on port 3001..."));
 
 // create a route
 app.get("/", (req, res) => {
@@ -17,3 +20,12 @@ app.get("/", (req, res) => {
 });
 
 module.exports = app;
+
+// connect to mongodb
+mongoose
+  .connect(
+    "mongodb+srv://fidelotieno11:zg7f=8GMr>DvuK=@cluster0.y2nouau.mongodb.net/?retryWrites=true&w=majority" ??
+      "MONGODB_URI is not defined"
+  )
+  .then(() => console.log("Connected to mongodb"))
+  .catch((err) => console.log(err));
